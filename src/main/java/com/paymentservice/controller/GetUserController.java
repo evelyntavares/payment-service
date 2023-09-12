28a6e03b299c5c94f67fb6d8790176c296956054
Evelyn Tavares <evelyn.tavares7@hotmail.com>
2023-09-12 14:47:13 -0600

    Create GetUserController

---
+++ src/main/java/com/paymentservice/controller/GetUserController.java
@@ -0,0 +1,36 @@
+package com.paymentservice.controller;
+
+import com.paymentservice.domain.user.User;
+import com.paymentservice.service.GetUserService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class GetUserController {
+
+  private final GetUserService getUserService;
+
+  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    final User user = getUserService.execute(id);
+    return new ResponseEntity<>(user, HttpStatus.CREATED);
+  }
+
+  @GetMapping(value = "/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<User> getUserById(@PathVariable String username) {
+    final User user = getUserService.execute(username);
+    return new ResponseEntity<>(user, HttpStatus.CREATED);
+  }
+
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<User>> getUsers() {
+    final List<User> users = getUserService.execute();
+    return new ResponseEntity<>(users, HttpStatus.OK);
+  }
+}
