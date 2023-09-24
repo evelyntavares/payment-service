52bbb441c7bc0cc4e89c796b609ec2a847b43ffe
Evelyn Tavares <evelyn.tavares7@hotmail.com>
2023-09-23 19:06:44 -0600

    Remove duplicated annotation.

--- src/test/java/com/paymentservice/controller/GetUserControllerTest.java
+++ src/test/java/com/paymentservice/controller/GetUserControllerTest.java
@@ -14,13 +14,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
 @AutoConfigureMockMvc
 class GetUserControllerTest extends IntegrationTestConfig {
   @MockBean private GetUserService getUserService;
