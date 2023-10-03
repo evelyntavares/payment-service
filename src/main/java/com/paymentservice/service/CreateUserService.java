3616104bce662a9b3b29879b9dcf1f2ceb7c9a42
Evelyn Tavares <evelyn.tavares7@hotmail.com>
2023-10-03 12:51:37 -0600

    Add CreateUserService interface

--- src/main/java/com/paymentservice/service/CreateUserService.java
+++ src/main/java/com/paymentservice/service/CreateUserService.java
@@ -1,27 +1,8 @@
 package com.paymentservice.service;
 
-import static com.paymentservice.util.NormalizeStringUtils.normalize;
-
 import com.paymentservice.domain.user.User;
 import com.paymentservice.dto.UserDTO;
-import com.paymentservice.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class CreateUserService {
-  private final UserRepository repository;
-
-  public User execute(UserDTO userDTO) {
-    final String username = normalize(userDTO.name());
-
-    final User user = new User();
-    user.setUsername(username);
-    user.setName(userDTO.name());
-    user.setBalance(userDTO.balance());
-    user.setEmail(userDTO.email());
-    user.setUserType(userDTO.userType());
-    return repository.save(user);
-  }
+public interface CreateUserService {
+  User execute(UserDTO userDTO);
 }
