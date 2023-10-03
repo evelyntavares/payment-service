8d2c5c97058d6297e25bc54c0ad95fe9b4228dce
Evelyn Tavares <evelyn.tavares7@hotmail.com>
2023-10-03 12:55:48 -0600

    Add GetUserService interface

---
+++ src/main/java/com/paymentservice/service/GetUserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.paymentservice.service;
+
+import com.paymentservice.domain.user.User;
+import com.paymentservice.repository.UserRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GetUserServiceImpl implements GetUserService {
+
+  private final UserRepository repository;
+
+  public User execute(Long id) {
+    return repository.findUserById(id).orElseThrow();
+  }
+
+  public List<User> execute() {
+    return repository.findAll();
+  }
+}
